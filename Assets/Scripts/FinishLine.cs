bb4b261dbd9b1c1325d5c2cadeccab2790f967d2
Tavin Draughon <trdraughon@gmail.com>
2026-08-08 11:31:30 -0700

    Revert "Finish Line, Restarting, Particles, QOL tweaks"
    This reverts commit 12c43907d9bcc8a8f103c41204880bbf73459939.

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,25 +1,9 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    [SerializeField] float delay = 1f;
-    [SerializeField] ParticleSystem finishParticles;
-
     void OnTriggerEnter2D(Collider2D collision)
     {
-        int layerIndex = LayerMask.NameToLayer("Player");
-
-        if (collision.gameObject.layer == layerIndex)
-        {
-            finishParticles.Play();
-            Invoke("ReloadScene", delay);
-        }
-    }
-
-    void ReloadScene()
-    {
-        SceneManager.LoadScene(0);
+        Debug.Log("Yes!!");
     }
 }
